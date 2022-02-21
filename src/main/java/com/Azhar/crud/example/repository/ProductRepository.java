e049b64c6380dbb269aad712a7e051aa5b454cd9
AzharMohd07 <79697475+AzharMohd07@users.noreply.github.com>
2022-02-21 12:08:49 +0530

    Update ProductRepository.java

--- src/main/java/com/Azhar/crud/example/repository/ProductRepository.java
+++ src/main/java/com/Azhar/crud/example/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.azhar.crud.example.repository;
 
-import com.javatechie.crud.example.entity.Product;
+import com.azhar.crud.example.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
